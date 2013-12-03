03616e39f2342a3a429235a641f76b7ed8d162a8
alfredo <alfredo@mola.com>
2013-12-03 13:24:57 +0100

    clase grafo

--- src/algoritmosdistribuidos/Grafo.java
+++ src/algoritmosdistribuidos/Grafo.java
@@ -6,10 +6,39 @@
 
 package algoritmosdistribuidos;
 
+import java.io.File;
+import java.util.*;
+
+
 /**
  *
  * @author pablo
  */
 public class Grafo {
+    AbstractList<Nodo> nodos;
     
+    public Grafo(Nodo raiz){
+        nodos.add(raiz);
+    }
+    public Grafo(AbstractList<Nodo> listaNodos){
+        nodos=listaNodos;        
+    }
+    
+    public Nodo getRaiz(){
+        return getNodo(0);        
+    }
+        
+    
+    //devuelve el nodo o null si no existe
+    public Nodo getNodo(int id){
+        Nodo nodo=null;
+        Iterator<Nodo> it= nodos.iterator();        
+        while(it.hasNext()){
+            Nodo n = it.next();
+            if (n.id==id){
+                nodo=n; break; //encontrado
+            }
+        }
+        return nodo;
+    }
 }
