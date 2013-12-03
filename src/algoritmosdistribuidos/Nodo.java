c5b3cc688acc8e4e86e4e221772bc8f71b4bfc23
pablo <pablo.riutort@gmail.com>
2013-12-03 13:04:55 +0100

    send y receive

--- src/algoritmosdistribuidos/Nodo.java
+++ src/algoritmosdistribuidos/Nodo.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package algoritmosdistribuidos;
 
 import java.util.AbstractList;
@@ -12,33 +11,55 @@ import java.util.AbstractList;
  *
  * @author pablo
  */
-public class Nodo extends Thread{
-    
-    private int id,idPadre,inDeficit,outDeficit;
-    private String mensaje;
-    //private cola; cola de mensajes
-    AbstractList <Integer> idPadres;
-    AbstractList <Integer> idHijos;
+public class Nodo extends Thread {
+
+    private int id, idPadre, inDeficit, outDeficit;
+    private int deudores[];
+    private boolean terminado;
+
+    AbstractList<Integer> idPadres;
+    AbstractList<Integer> idHijos;
 
     public Nodo(int id, int idPadre) {
         this.id = id;
         this.inDeficit = 0;
         this.outDeficit = 0;
         this.idPadre = idPadre;
+        this.terminado = false;
     }
 
     /**
-     * Método para enviar un mensaje de un nodo a otro.
-     * Se envía un mensaje de la cola de mensajes del nodo a otro nodo especi
-     * ficado por id. El outDeficit del nodo emisor aumenta.
-     * @param idDestino 
+     * Método para enviar un mensaje de un nodo a otro. Se envía un mensaje de
+     * la cola de mensajes del nodo a otro nodo especi ficado por id. El
+     * outDeficit del nodo emisor aumenta.
+     *
+     * @param idDestino
      */
-    public void sendMensj(String mensaje, int idReceptor, int myId){
+    public void sendMensj(String mensaje, int idReceptor, int myId) {
         //enviamos el mensaje al nodo indicado
         this.outDeficit++;
     }
-    
-    public void receiveMensj(String mensaje, int idEmisor){
+
+    public void receiveMensj(String mensaje, int idEmisor) {
         this.inDeficit++;
+    }
+
+    public boolean sendSignal(/*signal, E, */int myId) {
+        if ((this.inDeficit > 1) || ((this.inDeficit == 1) && (this.terminado) && (this.outDeficit == 0))) {
+            /*
+             E ← some edge E with inDeficit[E] = 0
+             send(signal, E, myID)
+             decrement inDeficit[E] and inDeficit
+             */
+            //send(signal, E, myId);
+            this.inDeficit--;
+            return true;
+        }
+        return false;
+    }
+
+    public void receiveSignal() {
+        receive(signal,_);
+        this.outDeficit--;
     }
 }
